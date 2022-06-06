326577c18c91807f4dc49cd659a73f54800ebd83
Lucas Mendes <lucasaraujo12345@outlook.com.br>
2022-06-06 11:37:36 -0300

    Exemplo 1

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exemplo 1</title>
+</head>
+<body>
+    <h1>PHP POO - Exemplo 1</h1>
+    <hr>
+
+    <h2>Assuntos abordados:</h2>
+    <ul>
+        <li>Criação da classe</li>
+        <li>Importação do arquivo da classe</li>
+        <li>Criação dos objetos</li>
+    </ul>
+
+<?php 
+// Importando a classe
+require_once "src/cliente.php";
+
+// Criação dos objetos
+$clienteA = new Cliente;
+$clienteB = new Cliente;
+?>    
+     <pre> <?=var_dump($clienteA, $clienteB)?></pre>
+</body>
+</html>
---
+++ src/cliente.php
@@ -0,0 +1,12 @@
+<?php
+
+class Cliente {
+    // Propriedade
+    public string $nome;
+    public string $email;
+    public string $senha;
+    public array $telefones;
+}
+
+
+?>

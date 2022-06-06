ce66e10b8d65b50b6b34a3ccd124f9da6281f3e4
Lucas Mendes <lucasaraujo12345@outlook.com.br>
2022-06-06 11:28:15 -0300

    exemplo

---
+++ exemplo-geral.php
@@ -0,0 +1,21 @@
+<?php
+// Exemplo geral PHP
+
+class Exemplo {
+    // Atributos ou Propriedades
+    public string $nome;
+    public string $escola; 
+    public string $cidade;
+}
+
+// Criar objetos/instancias
+$exemploA = new Exemplo;
+$exemploB = new Exemplo;
+
+echo "<pre>";
+var_dump($exemploA);
+var_dump($exemploB);
+echo "</pre>";
+
+
+?>
